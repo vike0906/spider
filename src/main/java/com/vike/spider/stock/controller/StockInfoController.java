af29d975d5223befd4535cc1728e9cdc05158669
liushengli <liujialong886@outlook.com>
2019-09-16 16:26:10 +0800

    init

---
+++ src/main/java/com/vike/spider/stock/controller/StockInfoController.java
@@ -0,0 +1,19 @@
+package com.vike.spider.stock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author: lsl
+ * @createDate: 2019/9/16
+ */
+@Controller
+@RequestMapping("stock-info")
+public class StockInfoController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "index";
+    }
+}
